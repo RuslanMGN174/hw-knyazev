985f0d3530d5f7158382f97454622cbe479c3568
rusKnyazev <rusland174@gmail.com>
2023-04-20 13:39:03 +0500

    my-site cleared from hw13.04

--- my-site/app/task4.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//use DateTime
-$date1 = new DateTime();;
-$date2 = DateTime::createFromFormat("d-m-Y", "22-10-1983");
-$interval = $date1->diff($date2);
-
-echo "Родился 22.10.1983г. Со дня моего рождения прошло " . $interval->days . " дней";
