10a366dd072295b362df058e2f4b32690437930e
rusKnyazev <rusland174@gmail.com>
2023-04-24 17:08:29 +0500

    bootstrap added for auth & reg forms

--- my-site/app/php/authorization.php
+++ my-site/app/php/authorization.php
@@ -17,28 +17,60 @@ if (isset($_POST["login"]) && isset($_POST["password"])) {
 }
 ?>
 
-<form action="" method="post">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body p-5">
+                            <h2 class="text-uppercase text-center mb-5">Authorization</h2>
 
-    <div class="form-outline mb-4">
-        <input type="text" id="form2Example1" class="form-control" name="login"/>
-        <label class="form-label" for="form2Example1">Login</label>
-    </div>
+                            <form action="" method="post">
 
-    <div class="form-outline mb-4">
-        <input type="password" id="form2Example2" class="form-control" name="password"/>
-        <label class="form-label" for="form2Example2">Password</label>
-    </div>
+                                <div class="form-outline mb-4">
+                                    <input type="text" id="form2Example1" class="form-control" name="login"/>
+                                    <label class="form-label" for="form2Example1">Login</label>
+                                </div>
 
-    <div class="row mb-4">
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="form2Example2" class="form-control" name="password"/>
+                                    <label class="form-label" for="form2Example2">Password</label>
+                                </div>
 
-        <div class="col">
-            <a href="#!">Forgot password?</a>
+                                <div class="row mb-4">
+
+                                    <div class="col">
+                                        <a href="#!">Forgot password?</a>
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                                <div class="text-center">
+                                    <p>Not a member? <a href="registration.php">Register</a></p>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+</section>
 
-    <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-    <div class="text-center">
-        <p>Not a member? <a href="registration.php">Register</a></p>
-    </div>
-</form>
+</body>
+</html>
--- my-site/app/php/registration.php
+++ my-site/app/php/registration.php
@@ -1,50 +1,65 @@
-<section class="vh-100 bg-image"
-<div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                <div class="card" style="border-radius: 15px;">
-                    <div class="card-body p-5">
-                        <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body p-5">
+                            <h2 class="text-uppercase text-center mb-5">Create an account</h2>
 
-                        <form>
+                            <form>
 
-                            <div class="form-outline mb-4">
-                                <input type="text" id="form3Example1cg" class="form-control form-control-lg"/>
-                                <label class="form-label" for="form3Example1cg">Your Name</label>
-                            </div>
+                                <div class="form-outline mb-4">
+                                    <input type="text" id="form3Example1cg" class="form-control form-control-lg"/>
+                                    <label class="form-label" for="form3Example1cg">Your Name</label>
+                                </div>
 
-                            <div class="form-outline mb-4">
-                                <input type="text" id="form3Example3cg" class="form-control form-control-lg"/>
-                                <label class="form-label" for="form3Example3cg">Your Email</label>
-                            </div>
+                                <div class="form-outline mb-4">
+                                    <input type="text" id="form3Example3cg" class="form-control form-control-lg"/>
+                                    <label class="form-label" for="form3Example3cg">Your Email</label>
+                                </div>
 
-                            <div class="form-outline mb-4">
-                                <input type="password" id="form3Example4cg" class="form-control form-control-lg"/>
-                                <label class="form-label" for="form3Example4cg">Password</label>
-                            </div>
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="form3Example4cg" class="form-control form-control-lg"/>
+                                    <label class="form-label" for="form3Example4cg">Password</label>
+                                </div>
 
-                            <div class="form-outline mb-4">
-                                <input type="password" id="form3Example4cdg" class="form-control form-control-lg"/>
-                                <label class="form-label" for="form3Example4cdg">Repeat your password</label>
-                            </div>
+                                <div class="form-outline mb-4">
+                                    <input type="password" id="form3Example4cdg" class="form-control form-control-lg"/>
+                                    <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                                </div>
 
-                            <div class="d-flex justify-content-center">
-                                <button type="button"
-                                        class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register
-                                </button>
-                            </div>
+                                <div class="d-flex justify-content-center">
+                                    <button type="button"
+                                            class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">
+                                        Register
+                                    </button>
+                                </div>
 
-                            <p class="text-center text-muted mt-5 mb-0">Have already an account? <a
-                                        href="authorization.php"
-                                        class="fw-bold text-body"><u>Login here</u></a></p>
+                                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a
+                                            href="authorization.php"
+                                            class="fw-bold text-body"><u>Login here</u></a></p>
 
-                        </form>
+                            </form>
 
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </section>
+</body>
+</html>
